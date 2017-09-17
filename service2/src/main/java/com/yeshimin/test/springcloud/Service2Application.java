1a1a27be8ceb077ab8ee6a38125620247755616d
yeshimin <ysm@yeshimin.com>
2017-09-17 19:06:32 +0800

    # Spring Cloud Feign DEBUG mode

--- service2/src/main/java/com/yeshimin/test/springcloud/Service2Application.java
+++ service2/src/main/java/com/yeshimin/test/springcloud/Service2Application.java
@@ -1,5 +1,6 @@
 package com.yeshimin.test.springcloud;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -19,6 +20,11 @@ public class Service2Application {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 
     public static void main(String[] args) {
